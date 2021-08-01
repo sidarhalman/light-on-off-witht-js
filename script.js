3e10e2ea4c2e15707c13f3d9c1c67447e0bc538a
sidarhalman <sidarhalman93@gmail.com>
2021-08-01 20:18:30 +0200

    completed

---
+++ script.js
@@ -0,0 +1,17 @@
+function onCss() {
+    document.getElementById('box1').style.backgroundColor = "#00ff00";
+    document.getElementById('box1').style.boxShadow = "5px 5px 20px"
+    document.getElementById('box1').style.boxShadowColor = "#00ff00"
+    document.getElementById('box1').style.border = "none";
+};
+
+function offCss() {
+    document.getElementById('box1').
+    style.boxShadow = "none";
+
+    document.getElementById('box1').
+    style.backgroundColor = "#000000";
+
+    document.getElementById('box1').
+    style.border = "1px solid darkgreen"
+}
